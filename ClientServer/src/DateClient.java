72f21f6f303d03fa6bebc632750f7348904856c3
Pfeiffy <dgmm@gmx.de>
2016-10-14 12:48:33 +0200

    Update DateClient.java

--- ClientServer/src/DateClient.java
+++ ClientServer/src/DateClient.java
@@ -15,15 +15,19 @@ public class DateClient {
 	 * Runs the client as an application. First it displays a dialog box asking
 	 * for the IP address or hostname of a host running the date server, then
 	 * connects to it and displays the date that it serves.
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Socket s = new Socket("localhost", 9090);
 		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-		// das gibt er zum Server rüber
-		out.println("$111112222");
-		BufferedReader input = new BufferedReader(new InputStreamReader(
-				s.getInputStream()));
-		System.out.println(input.readLine());
-		System.exit(0);
+		// das gibt er zum Server rÃ¼ber
+		for (int count = 0; count < 10; count++) {
+			out.println("$111112222:" + count);
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					s.getInputStream()));
+			System.out.println("Client: " + input.readLine());
+			Thread.sleep(500);
+		}
+		// System.exit(0);
 	}
 }
